f1e45d555d6db1abafe1e72ade58711c1ee50909
johnz17 <Johnz17u@gmail.com>
2024-08-14 22:11:20 -0400

    First Sample mod thing 2

--- src/main/java/Nightlight/mods/SunlightOres112.java
+++ src/main/java/Nightlight/mods/SunlightOres112.java
@@ -1,4 +1,4 @@
-package com.example;
+package Nightlight.mods;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -6,17 +6,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(
-	modid = SampleMod112.MODID,
-	name = SampleMod112.NAME,
-	version = SampleMod112.VERSION
+	modid = SunlightOres112.MODID,
+	name = SunlightOres112.NAME,
+	version = SunlightOres112.VERSION
 )
-public class SampleMod112 {
-	public static final String MODID = "samplemod112";
-	public static final String NAME = "Sample Mod 1.12";
+public class SunlightOres112 {
+	public static final String MODID = "sunlightores";
+	public static final String NAME = "SunlightOres 1.12";
 	public static final String VERSION = "1.0";
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
-	
 	@Mod.EventHandler
 	public void preinit(FMLPreInitializationEvent preinit) {
 		LOGGER.info("Hello, world!");
